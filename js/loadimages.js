e4e3892620b7a520a8882123e441a301336d32e6
Henry Spivey <spivotron@gmail.com>
2015-10-11 10:57:14 -0700

    trying to load image with ajax

--- js/loadimages.js
+++ js/loadimages.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	
 	function displayPhotos(data) {
 		var photoHTML = "<ul>";
-		console.log(data.items);
+		console.log(Object.keys(data.items));
 		$.each(data.items, function(i, photo) {
 			
 
