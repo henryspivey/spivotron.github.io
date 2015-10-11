e326f383f25db9301160a464ea3c868ca1855e70
Henry Spivey <spivotron@gmail.com>
2015-10-11 01:47:18 -0700

    trying to load image with ajax

--- js/loadimages.js
+++ js/loadimages.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
 	var images = "https://raw.githubusercontent.com/spivotron/spivotron.github.io/master/images/images.json";
+	
 	function displayPhotos(data) {
-		console.log(data);
+		var photoHTML = "<ul>";
+		$.each(data.items, function(i, photo) {
+			photoHTML += "<li>";
+			photoHTML += "<img src = "+data.src + ">";
+			photoHTML += "<p>"+ data.caption + "</p>";
+			photoHTML += "</li>";
+		});
+		photoHTML += "</ul>";
+		$("#gallery").html("photoHTML");
 	}
 	$.getJSON(images, displayPhotos);
 
